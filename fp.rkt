ad0327441edbe5f3190ed877ed87fe3f48ee3e3c
sammanthp007 <samman.thapa@bison.howard.edu>
2017-01-23 12:01:27 -0500

    implement number 1 with flawed solution

--- fp.rkt
+++ fp.rkt
@@ -2,4 +2,22 @@
 ;;; edu) as part of an assignment for CSCI 350: Structures of Programming
 ;;; Language at Howard University.
 
+(define (nmember atm lis)
+  (cond
+    ((NULL? lis) #F) ;; empty list
+    ((EQ? atm (CAR lis)) #T)
+    (ELSE (nmember atm (CDR lis)))
+    )
+  )
 
+;;; 1. (25 pts) Write a function (reverse-general L). L is a list. The result
+;;; of the function is the reversed version of L. Every single sub-list in L
+;;; should be reversed as well. For example, the result of
+;;; (reverse-general ‘(a b (c (d e)) f) should be (f ((e d) c) b a).
+
+(define (reverse-general L)
+  (cond
+    ((NULL? L) '())
+    (else (CONS (reverse-general (CDR L)) (CAR L)))
+    )
+  )
