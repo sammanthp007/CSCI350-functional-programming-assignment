1eb13d254b5ef48cdf0365ecf60dab10f48256db
sammanthp007 <samman.thapa@bison.howard.edu>
2017-01-23 22:08:06 -0500

    implement number 2: write the logic for the solution in scheme

--- fp.rkt
+++ fp.rkt
@@ -22,9 +22,21 @@
 
 
 
-;;; (25 pts) Write a function (sum-up-numbers-simple L). L is a list, which 
+;;; (25 pts) Write a function (sum-up-numbers-simple L). L is a list, which
 ;;; may contain as elements numbers and non-numbers. The result of the function
-;;; is the sum of the numbers not in nested lists in L. If there are no such 
-;;; numbers, the result is zero. For example, the result of 
+;;; is the sum of the numbers not in nested lists in L. If there are no such
+;;; numbers, the result is zero. For example, the result of
 ;;; (sum-up-numbers-simple ‘(a b 1 2 c 3 d)) should be 6.
 
+(define (sum-up-numbers-simple L)
+  (cond
+    ; if L is empty list
+    ((null? L) '0)
+    ; if first element is list
+    ((list? (car L)) (sum-up-numbers-simple(cdr L)))
+    ; if first element is not a number
+    ((not (number? (car L))) (sum-up-numbers-simple(cdr L)))
+    ; calculate recursively
+    (else (+ (car L) (sum-up-numbers-simple(cdr L)))
+          ))
+  )
