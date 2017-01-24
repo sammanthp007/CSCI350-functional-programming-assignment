8394e4c7f44685414c3b7b825f7c725fbd10ce84
sammanthp007 <samman.thapa@bison.howard.edu>
2017-01-23 22:59:36 -0500

    implement 4: copy and paste the question number 4, will start working on it from tomorrow

--- fp.rkt
+++ fp.rkt
@@ -85,3 +85,14 @@
 ;(sum-up-numbers-general '(100 (200)))
 ;(sum-up-numbers-general '(a 100 b (200) c 300 d))
 ;(sum-up-numbers-general '(a 100 ((b ((200) c)) 300 d)))
+
+;;; 4. (25 pts) Write a function (min-above-min L1 L2). L1 and L2 are both 
+;;; simple lists, which do not contain nested lists. Both lists may have 
+;;; non-numeric elements. The result of the function is the minimum of the 
+;;; numbers in L1 that are larger than the smallest number in L2. If there is 
+;;; no number in L2, all the numbers in L1 should be used to calculate the 
+;;; minimum. If there is no number in L1 larger than the smallest number in L2, 
+;;; the result is false (#F). For example, the result of 
+;;; (min-above-min ‘(2 a 1 3) ‘(b 5 3 1)) should be 2.
+
+
