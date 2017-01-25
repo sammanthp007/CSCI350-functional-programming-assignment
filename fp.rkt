3c6ebd7f6d0b3ae0fb99c8a342606dedc9421050
sammanthp007 <samman.thapa@bison.howard.edu>
2017-01-24 22:42:37 -0500

    refactor

--- fp.rkt
+++ fp.rkt
@@ -7,9 +7,12 @@
 ;;; of the function is the reversed version of L. Every single sub-list in L
 ;;; should be reversed as well. For example, the result of
 ;;; (reverse-general ‘(a b (c (d e)) f) should be (f ((e d) c) b a).
+
 (define (reverse-general L)
   (cond
+    ; if L is empty, return empty
     ((NULL? L) '())
+    ; else return reverse received from recursive evaluation
     (else (append (reverse-general (CDR L)) ((lambda(x)
                                                (if (LIST? x)
                                                  (LIST (reverse-general x))
@@ -20,10 +23,8 @@
     )
   )
 
-;(reverse-general '(a b c (d e f) g h))
 
-
-;;; (25 pts) Write a function (sum-up-numbers-simple L). L is a list, which
+;;; 2. (25 pts) Write a function (sum-up-numbers-simple L). L is a list, which
 ;;; may contain as elements numbers and non-numbers. The result of the function
 ;;; is the sum of the numbers not in nested lists in L. If there are no such
 ;;; numbers, the result is zero. For example, the result of
@@ -31,32 +32,18 @@
 
 (define (sum-up-numbers-simple L)
   (cond
-    ; if L is empty list
+    ; if L is empty list return 0
     ((null? L) '0)
-    ; if first element is list
+    ; if first element is list ignore it and calculte sum of rest of the list
     ((list? (car L)) (sum-up-numbers-simple(cdr L)))
-    ; if first element is not a number
+    ; if first element is not a number, ignore and calculate sum for rest
     ((not (number? (car L))) (sum-up-numbers-simple(cdr L)))
     ; calculate recursively
-    (else (+ (car L) (sum-up-numbers-simple(cdr L)))
-          ))
+    (else (+ (car L) (sum-up-numbers-simple(cdr L))))
+    )
   )
 
-; (sum-up-numbers-simple '())
-; (sum-up-numbers-simple '(100 200))
-; (sum-up-numbers-simple '(a b c) )
-; (sum-up-numbers-simple '(100 a))
-; (sum-up-numbers-simple '(a 100))
-; (sum-up-numbers-simple '(a 100 b 200 c 300 d))
-; (sum-up-numbers-simple '(()))
-; (sum-up-numbers-simple '((100)))
-; (sum-up-numbers-simple '(100 (200)))
-; (sum-up-numbers-simple '(a 100 b (200) c 300 d))
-; 
-; (sum-up-numbers-simple '(100 (200) (a) a b a c 200 (2)))
-; (sum-up-numbers-simple '((((3)))))
-
-;;; (25 pts) Write a function (sum-up-numbers-general L). L is a list, which 
+;;; 3. (25 pts) Write a function (sum-up-numbers-general L). L is a list, which 
 ;;; may contain as elements numbers and non-numbers. The result of the function
 ;;; is the sum of all the numbers (including those in nested lists) in L. If 
 ;;; there are no such numbers, the result is zero. For example, the result of 
@@ -64,27 +51,18 @@
 
 (define (sum-up-numbers-general L)
   (cond
-    ; if L is empty list
+    ; if L is empty list, return 0
     ((null? L) '0)
-    ; if first element is list
-    ((list? (car L)) (+ (sum-up-numbers-general (car L)) (sum-up-numbers-general(cdr L))))
-    ; if first element is not a number
+    ; if first element is list, calculate sum for both CAR L and CDR L
+    ((list? (car L)) (+ (sum-up-numbers-general (car L)) 
+                        (sum-up-numbers-general(cdr L))))
+    ; if first element is not a number, ignore CAR L and calculate for rest
     ((not (number? (car L))) (sum-up-numbers-general(cdr L)))
-    ; calculate recursively
-    (else (+ (car L) (sum-up-numbers-general(cdr L)))
-          ))
+    ; calculate recursively sum of CAR L and CDR L
+    (else (+ (car L) (sum-up-numbers-general(cdr L))))
+    )
   )
 
-;(sum-up-numbers-general '() )
-;(sum-up-numbers-general '(100) )
-;(sum-up-numbers-general '(100 200)  )
-;(sum-up-numbers-general '(a) )
-;(sum-up-numbers-general '(a 100 b 200 c 300 d) )
-;(sum-up-numbers-general '(()))
-;(sum-up-numbers-general '((100)))
-;(sum-up-numbers-general '(100 (200)))
-;(sum-up-numbers-general '(a 100 b (200) c 300 d))
-;(sum-up-numbers-general '(a 100 ((b ((200) c)) 300 d)))
 
 ;;; 4. (25 pts) Write a function (min-above-min L1 L2). L1 and L2 are both 
 ;;; simple lists, which do not contain nested lists. Both lists may have 
@@ -95,6 +73,7 @@
 ;;; the result is false (#F). For example, the result of 
 ;;; (min-above-min ‘(2 a 1 3) ‘(b 5 3 1)) should be 2.
 
+;;; returns a list of numbers from lis larger than val
 (define (greater_than lis val output)
   (cond
     ; if car lis = null then do nothing
@@ -102,15 +81,15 @@
                            (cons (car lis) output)
                            output))
     ; if car lis > val then append to output
-    ((and (number? (car lis)) (> (car lis) val)) (greater_than (cdr lis) val (cons (car lis) output)))
+    ((and (number? (car lis)) (> (car lis) val)) 
+     (greater_than (cdr lis) val (cons (car lis) output)))
     ; if car lis <= val then do nothing
     (else (greater_than (cdr lis) val output))
+    )
   )
-)
 
 ;;; returns the minimum number as an atom of the list
 (define (min_atm_of lis)
-  ; if the first atm is a number then 
   (cond
     ; if lis is empty return empty 
     ((null? lis) #f)
@@ -122,20 +101,21 @@
     ((< (car lis) (min_atm_of(cdr lis))) (car lis))
     ; else return min from the remaining list
     (else (min_atm_of(cdr lis)))
+    )
   )
-)
 
 
 (define (min-above-min L1 L2)
   (cond
-    ; If there is no number in L2, all the numbers in L1 should be used to calculate the minimum
+    ; If there is no number in L2, all the numbers calculate min of L1
     ((null? L2) (min_atm_of L1))
     ((null? L1) #f)
     (else (cond
-            ; if 
+            ; if no element in L1 is larger than smallest of L2, return false
             ((null? (min_atm_of ( greater_than L1 (min_atm_of L2) '()))) #f)
+            ; else find min from list of numbers in L1 larger than min of l2
             (else (min_atm_of ( greater_than L1 (min_atm_of L2) '())))
             )
-     )
+          )
+    )
   )
-)
